f85725bc7758557390f64d7adfa0c69d4566ac7a
Niphoros-Tech-TA <admin@niphoros.tech>
2023-05-22 13:11:09 +0300

    importing the image in the VM

--- proxmox/cloud-init/vars.sh
+++ proxmox/cloud-init/vars.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 imgPath="/root/personal-scripts/proxmox/cloud-init/ubuntu-images"
-# http://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
+cloudImages="https://cloud-images.ubuntu.com/"
